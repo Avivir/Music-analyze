7c1547a9c8c20e7f7b54ac143f0b961ead643036
Adam <adamwieczorek36@gmail.com>
2024-12-22 17:32:41 +0100

    gotowy projekt

--- src/main/java/com/example/Database/DatabaseConnection.java
+++ src/main/java/com/example/Database/DatabaseConnection.java
@@ -1,32 +1,77 @@
 package com.example.Database;
+import com.example.API.Friends;
+import com.example.API.User;
+
 import java.sql.Connection;
 import java.sql.DriverManager;
 import java.sql.PreparedStatement;
 import java.sql.ResultSet;
 import java.sql.SQLException;
 
+import java.sql.*;
+
+import java.sql.*;
+import java.util.List;
+
 public class DatabaseConnection {
 
-    private static final String URL = "jdbc:postgresql://localhost:5432/postgres";  // Zmienna URL bazy
-    private static final String USER = "postgres";
-    private static final String PASSWORD = "postgres";
+    private static final String URL = "jdbc:postgresql://localhost:5432/postgres";
+    private static final String USER = "postgres"; // Zmień na swoją nazwę użytkownika
+    private static final String PASSWORD = "postgres"; // Zmień na swoje hasło
 
-    public static void main(String[] args) {
-        try (Connection connection = DriverManager.getConnection(URL, USER, PASSWORD)) {
+    // Metoda do nawiązania połączenia
+    public static Connection getConnection() throws SQLException {
+        return DriverManager.getConnection(URL, USER, PASSWORD);
+    }
 
-            String query = "SELECT * FROM lastfmusers";
-            try (PreparedStatement statement = connection.prepareStatement(query);
-                 ResultSet resultSet = statement.executeQuery()) {
+    public static void batchInsert(Connection connection, List<User> users) {
+        String insertQuery = "INSERT INTO users (name, country, isuseractive, mostlistenedmusic, listofartist, numberofplaycounts) VALUES (?, ?, ? , ? , ?, ?)";
 
-                while (resultSet.next()) {
-                    String name = resultSet.getString("username");  // Załóżmy, że masz kolumnę "name"
-                    System.out.println(name);
+        try (PreparedStatement statement = connection.prepareStatement(insertQuery)) {
+            int count = 0;
+
+            for (User user : users) {
+                statement.setString(1, user.getName());
+                statement.setString(2, user.getCountry());
+                statement.setBoolean(3, user.getIsUserActive());
+                statement.setString(4, user.getMostListenedMusic().toString());
+                statement.setString(5, user.getListOfArtist().toString());
+                statement.setString(6, user.getNumberOfPlaycounts().toString());
+
+                statement.addBatch();
+
+                if (++count % 1000 == 0) {
+                    statement.executeBatch();
                 }
+            }
+
+            statement.executeBatch(); // Wykonaj pozostałe wpisy
+            System.out.println("Dane zostały pomyślnie wstawione do tabeli w batchach.");
+
+        } catch (SQLException e) {
+            e.printStackTrace();
+            System.out.println("Błąd podczas wstawiania danych do tabeli.");
+        }
+    }
+
+    // Metoda do wyciągania danych
+    public static void fetchData() {
+        String selectQuery = "SELECT * FROM users";
+
+        try (Connection connection = getConnection();
+             PreparedStatement statement = connection.prepareStatement(selectQuery);
+             ResultSet resultSet = statement.executeQuery()) {
+
+            System.out.println("Dane w tabeli users:");
+            while (resultSet.next()) {
+                String name = resultSet.getString("name");
+                int age = resultSet.getInt("age");
+                System.out.println("Name: " + name + ", Age: " + age);
             }
 
         } catch (SQLException e) {
             e.printStackTrace();
-            System.out.println("Błąd połączenia z bazą danych.");
+            System.out.println("Błąd podczas wyciągania danych z tabeli.");
         }
     }
 }
--- src/main/java/com/example/Main.java
+++ src/main/java/com/example/Main.java
@@ -1,6 +1,7 @@
 package com.example;
 import com.example.API.LastFmData;
 import com.example.API.User;
+import com.example.Database.DatabaseConnection;
 
 import java.util.List;
 
@@ -8,19 +9,39 @@ public class Main {
     public static void main(String[] args) {
 
         LastFmData lastData = new LastFmData("c8b3d6a3ab8ee34be63041d024194261");
-        List<User> friends = lastData.getUserFriendsWithStatus("music");
 
-        //for(User friend : friends) {
-          //  System.out.println(friend.getName());
-            //System.out.println(friend.getCountry());
-            //System.out.println(friend.getIsUserActive());
-            //int index = 0;
-            //for(String track : friend.getMostListenedMusic()){
-              //  System.out.println(track + " " + index);
-                //System.out.println(friend.getNumberOfPlaycounts().get(index));
-               // System.out.println(friend.getListOfArtist().get(index));
-                //index++;
-            //}
-       // }
+        List<User> friends = lastData.getUserFriendsWithStatus("rj");
+
+        friends.forEach(w -> {
+            List<User> friends2 = lastData.getUserFriendsWithStatus(w.getName());
+          if (Objects.nonNull(friends2) && friends2.size() > 0) {
+              friends.add(w);
+              System.out.println(friends.size());
+                  friends2.forEach(w2 -> {
+                      List<User> friends3 = lastData.getUserFriendsWithStatus(w.getName());
+                      if (Objects.nonNull(friends3) && friends3.size() > 0) {
+                      friends.add(w2);
+                      System.out.println(friends.size());
+                      friends3.forEach(w3 -> {
+                          List<User> friends4 = lastData.getUserFriendsWithStatus(w2.getName());
+                          if (Objects.nonNull(friends4) && friends4.size() > 0) {
+                              friends.add(w3);
+                              System.out.println(friends.size());
+//                              friends4.forEach(w4 -> {
+//                                  List<User> friends5 = lastData.getUserFriendsWithStatus(w2.getName());
+//
+//                              });
+                          }
+                      });
+                      }
+                  });
+          }
+        });
+
+        System.out.println("dupa");
+
+//        Connection connection = databaseConnection.getConnection();
+//
+//        databaseConnection.batchInsert(connection, friends);
+        }
     }
-}
